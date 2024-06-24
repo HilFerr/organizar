a67d5868019aa4dfdcf6bb5624654c78ca154546
HilFerr <157055839+HilFerr@users.noreply.github.com>
2024-06-24 11:32:35 +0100

    Add files via upload

---
+++ JAVA WEB/JAVA WEB/js/script_sobre.js
@@ -0,0 +1,6 @@
+//console.log("Acabei de ler o script_sobre.js")
+document.getElementById0("headerSobreMim").innerHTML = "Ines Campos"
+//document.getElementById("txt").innerHTML = "aiaiai";
+//$(document).ready(function(){
+    $(".txt").html("aiaiai");
+//});
