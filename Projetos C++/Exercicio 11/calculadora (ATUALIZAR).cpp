c4ccf9632aa2a85b98d8c0268d55e2ab2ba114f7
HilFerr <157055839+HilFerr@users.noreply.github.com>
2024-09-30 12:28:02 +0100

    Update and rename ex11.cpp to calculadora (ATUALIZAR).cpp

--- Projetos C++/Exercicio 11/calculadora (ATUALIZAR).cpp
+++ Projetos C++/Exercicio 11/calculadora (ATUALIZAR).cpp
@@ -1,42 +1,20 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-#include <stdio.h>  /* printf, scanf, puts, NULL */
 
 using namespace std;
 
-double resultado=0;
+double resultado = 0;
 
-void soma () {
-
-    int num1, num2;
-
-    cout << "*** SOMA DE 2 NUMEROS INTEIROS \n";
-    cout << "1.NUM \n";
-    cin >> num1;
-    cout << "2.NUM \n";
-    cin >> num2;
-
+void soma(num1, num2) {
     cout << "SOMA = " << (num1 + num2) << endl << endl;
 }
 
-void subtrair () {
-
-    int num1, num2;
-
+void subtrair(num1, num2) {
     cout << "SUBTRACAO = " << (num1 - num2) << endl << endl;
 }
 
-double dividir() {
-
-    int num1, num2;
-
-    cout << "*** DIVISAO DE 2 NUMEROS INTEIROS \n";
-    cout << "1.NUM \n";
-    cin >> num1;
-    cout << "2.NUM \n";
-    cin >> num2;
-
+double dividir(num1, num2) {
     return (double)num1/(double)num2;
 }
 
@@ -49,24 +27,29 @@ int main () {
     char op = '0';
 
     do {
-        printf("Qual a operacao que deseja efetuar ? \n");
-        printf("1 - Somar \n");
-        printf("2 - Subtrair \n");
-        printf("3 - Multiplicar \n");
-        printf("4 - Dividir \n");
-        printf("0 - Fechar App \n");
-        scanf("%d", &op);
+        // User Interactive Form
+        cout << "Qual a operacao que deseja efetuar? \n";
+        cout << "1 - Somar \n";
+        cout << "2 - Subtrair \n";
+        cout << "3 - Multiplicar \n";
+        cout << "4 - Dividir \n";
+        cout << "0 - Fechar App \n";
+        cin >> op;
+
+        system("cls");
 
         switch (op) {
-
+            // Soma
             case 1:
                 soma();
                 break;
 
+            // Subtrair
             case 2:
                 subtrair();
                 break;
 
+            // Multiplicação
             case 3:
                 int num1, num2;
                 cout << "*** MULTIPLICACAO DE 2 NUMEROS INTEIROS \n";
@@ -78,12 +61,12 @@ int main () {
                 cout << "\nResultado: " << resultado << endl << endl;
                 break;
 
+            // Dividir
             case 4:
                 resultado = dividir();
                 cout << "\nResultado: " << resultado << endl << endl;
                 break;
         }
-
     } while (op != '0');
 
     return 0;
